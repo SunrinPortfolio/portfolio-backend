6327acbd62c4e279507315dbfc886eecdcb5b264
okms00909 <okms00909@gmail.com>
2018-09-04 03:49:36 +0900

    change inadequate string for rate

--- src/data/modules/contestInfoToString.js
+++ src/data/modules/contestInfoToString.js
@@ -1,50 +1,64 @@
 
 const contestInfoToString = data => {
-  let contestName = '';
+  let type = '';
   let field = '';
   let rate = '';
 
   const { contestInfo } = data;
-  switch (contestInfo.type.main) {
+  switch (contestInfo.type) {
     case 1:
-    contestName = '디지털 콘텐츠 경진대회';
-    switch (contestInfo.type.sub) {
-      case 1: field = '응용'; break;
-      case 2: field = '웹 콘텐츠'; break;
-      case 3: field = '멀티미디어'; break;
-    }
-    switch (contestInfo.rate) {
-      case 1: rate = '대상'; break;
-      case 2: rate = '금상'; break;
-    }
+    type = '디지털 콘텐츠 경진대회';
     break;
     
     case 2:
-    contestName = '모바일 콘텐츠 경진대회';
-    switch (contestInfo.type.sub) {
-      case 1: field = '게임'; break;
-      case 2: field = '생활'; break;
-    }
-    switch (contestInfo.rate) {
-      case 1: rate = '대상'; break;
-      case 2: rate = '금상'; break;
-    }
+    type = '모바일 콘텐츠 경진대회';
     break;
     
     case 3:
-    contestName = '선린 해커톤';
-    switch (contestInfo.type.sub) {
-      case 1: field = '게임'; break;
-      case 2: field = '생활'; break;
-    }
-    switch (contestInfo.rate) {
-      case 1: rate = '금상'; break;
-    }
+    type = '선린 해커톤';
+    break;
+  }
+  switch (contestInfo.field) {
+    case 1:
+    field = '게임';
+    break;
+    
+    case 2:
+    field = '생활';
+    break;
+    
+    case 3:
+    field = '응용';
+    break;
+    
+    case 4:
+    field = '웹 콘텐츠';
+    break;
+    
+    case 5:
+    field = '멀티미디어';
+    break;
+  }
+  switch (contestInfo.rate) {
+    case 1:
+    rate = '대상';
+    break;
+    
+    case 2:
+    rate = '금상';
+    break;
+
+    case 3:
+    rate = '은상';
+    break;
+
+    case 4:
+    rate = '동상';
     break;
   }
 
   return {
-    contest: contestName,
+    type,
     field,
     rate,
   };
