6b588a5a8de9c50b692597737eb6873188c5568a
okms00909 <okms00909@gmail.com>
2018-09-04 04:16:06 +0900

    fix error

--- src/data/modules/getDataFileDirName.js
+++ src/data/modules/getDataFileDirName.js
@@ -1,4 +1,4 @@
 
-const getDataFileDirName = data => (String(data.projectName.replace(/[:%^&()!?]/gi, '').replace(/\s/gi, '_')));
+const getDataFileDirName = data => (String(data.projectName.replace(/[~:%^&()!?]/gi, '').replace(/\s+/gi, '_')));
 
 export default getDataFileDirName;
